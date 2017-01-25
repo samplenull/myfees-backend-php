0528fa0cb945d591f34aa9f39e119e98b2b937ca
Denis Kazimirov <samplenull@gmail.com>
2017-01-26 00:53:10 +0200

    New\Post utility implementation

--- src/AppBundle/Controller/UtilityController.php
+++ src/AppBundle/Controller/UtilityController.php
@@ -2,8 +2,11 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Utility;
+use AppBundle\Form\UtilityType;
 use FOS\RestBundle\Controller\FOSRestController;
 use Nelmio\ApiDocBundle\Annotation\ApiDoc;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * Class UtilityController
@@ -63,19 +66,29 @@ class UtilityController extends FOSRestController
         return $this->handleView($this->view($data, 200));
     }
 
-    public function postUtilitiesAction()
+    public function postUtilitiesAction(Request $request)
     {
-        
+        $data = json_decode($request->getContent(), true);
+        $utility = new Utility();
+        $form = $this->createForm(UtilityType::class, $utility);
+        $form->submit($data);
+
+        if ($form->isValid()) {
+            $em = $this->get('doctrine')->getManager();
+            $em->persist($utility);
+            $em->flush();
+            return $this->handleView($this->routeRedirectView('get_utility', ['id' => $utility->getId()]));
+        }
+        return ['form' => $form->getErrors()]; // ?? maybe not the right return here
+
     }
 
-    public function putUtilitiesAction($id)
+    public function putUtilityAction($id)
     {
-
     }
 
-    public function deleteUtilitiesAction()
+    public function deleteUtilityAction()
     {
-
     }
 
 
